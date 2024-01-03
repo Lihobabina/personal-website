f151c74b7be9ddc3b6b35088130bdac04ebf607c
Olya <starshova1309@gmail.com>
2024-01-03 19:53:35 +0200

    commit

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+window.addEventListener('load', function(event) {
+  setTimeout(function() {
+      var preloader = document.getElementById('preloader');
+      preloader.style.transition = 'opacity 1.3s';
+      preloader.style.opacity = 0;
+      setTimeout(function() {
+          preloader.style.opacity = '0';
+      }, 1300);
+  }, 1300);
+});
+
 
 
 document.addEventListener('mousemove', function (e) {
